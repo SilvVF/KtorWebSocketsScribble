bb007b86349725b63e0ee2bbeb3f45554a85f092
SilvVF <david.silver232@gmail.com>
2022-07-02 10:47:41 -0400

    Handle Game Flow

--- src/main/kotlin/com/silvvf/data/Room.kt
+++ src/main/kotlin/com/silvvf/data/Room.kt
@@ -9,6 +9,32 @@ class Room(
     var maxPlayers: Int,
     var players: List<Player> = emptyList()
 ) {
+    init {
+        //what to do when the phase setter is accessed handles the change
+        setPhaseChangedListener {phase ->
+            when (phase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.NEW_ROUND -> newRound()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            //only one thread at a time can access this setter
+            synchronized(field) {
+                field = value
+                phaseChangedListener?.let {change ->
+                    change(value)
+                }
+            }
+        }
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit) {
+        phaseChangedListener = listener
+    }
     //used to update the chat for all players in the game
     suspend fun broadcast(message: String) {
         players.forEach {player ->
@@ -35,5 +61,28 @@ class Room(
         }
         return false
     }
+    private fun waitingForPlayers() {
+
+    }
+    private fun waitingForStart(){
+
+    }
+    private fun newRound(){
+
+    }
+    private fun gameRunning(){
+
+    }
+    private fun showWord(){
+
+    }
+    enum class Phase{
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
+    }
+
 }
 
